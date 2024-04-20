98626407e2035d5f151b3670248c30d8dd49b1c6
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2024-04-20 06:48:05 +0200

    Update check1.R

--- R/check1.R
+++ R/check1.R
@@ -9,32 +9,35 @@
     is.integer2 = function(x) all(floor(x)==x)
   
 
-#2) Check 1 function
+#2) check1(): function that evaluates type and length of a given argument in the function
       check1 = function(var,varname, length.check, type.check)
       {
         
-        #Check legnth
+        #Check length
           if (length(var)!=length.check) {
-              exit("interprobe() says the variable '",varname,"' must be of length '",length.check,"' but it is of length '",length(var),"'")  
+              exit("interprobe() says the argument '",varname,"' must be of length '",length.check,"'\nbut it is of length '",length(var),"'")  
           }
             
       
         #Check type integer
           if (type.check=='integer')
             {
-            if (is.integer2(var)==FALSE) exit("interprobe() says the variable '",varname,"' must be an integer, but '",var, "' is not an integer.")  
-          }
+            if (is.integer2(var)==FALSE) {
+              exit("interprobe() says the argument '",varname,"' must be an integer, but '",var, "' isn't.")  
+            }
+              
+              }
         
         #Check type character
           if (type.check=='character')
             {
-            if (is.character(var)==FALSE) exit("interprobe() says the variable '",varname,"' must be a character variable but '",var, "' is not a character variable")  
+            if (is.character(var)==FALSE) exit("interprobe() says the argument '",varname,"' must be a character variable but '",var, "' isn't.")  
           }
         
         #Check type numeric
           if (type.check=='numeric')
             {
-            if (is.numeric(var)==FALSE) exit("interprobe() says the variable '",varname,"' must be a numeric, but '",var, "' is not numeric")  
+            if (is.numeric(var)==FALSE) exit("interprobe() says the argument '",varname,"' must be a numeric, but '",var, "' isn't.")  
           }
         
         
