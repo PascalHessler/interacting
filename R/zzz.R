2efc05192caffc758f89fc398d246cc914de88a1
Uri Simonsohn <urisohn@gmail.com>
2024-05-16 13:32:48 +0200

    add probe bins

--- R/zzz.R
+++ R/zzz.R
@@ -14,7 +14,7 @@
         
       #While developing:
          packageStartupMessage ("#######################################################\n",
-                                "interacting v.0.1.0  DEV version: 2024 05 14 [651 - BCN]")
+                                "interacting v.0.1.0  DEV version: 2024 05 16 [1329 - BCN]")
 
   } #End on attach
     
